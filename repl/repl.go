04f9c56053012831ed3701e512dd6a0f54cd6688
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-08-17 23:45:52 +0530

    feat: add evaluator in repl package

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"loco/evaluator"
 	"loco/lexer"
 	"loco/parser"
 )
@@ -30,8 +31,13 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+
 	}
 }
 
