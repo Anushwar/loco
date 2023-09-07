8c4e9c0a5065b94b0ab7430f6d92229df225743e
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-09-07 23:42:53 +0530

    test: extend TestReturnStatements

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -115,11 +115,11 @@ func TestReturnStatements(t *testing.T) {
 		input    string
 		expected int64
 	}{
-
 		{"return 10;", 10},
 		{"return 10; 9;", 10},
 		{"return 2 * 5; 9;", 10},
 		{"9; return 2 * 5; 9;", 10},
+		{"if (10 > 1) { return 10; }", 10},
 		{
 			`
 if (10 > 1) {
@@ -131,6 +131,25 @@ if (10 > 1) {
 }
 `,
 			10,
+		},
+		{
+			`
+let f = fn(x) {
+  return x;
+  x + 10;
+};
+f(10);`,
+			10,
+		},
+		{
+			`
+let f = fn(x) {
+   let result = x + 10;
+   return result;
+   return 10;
+};
+f(10);`,
+			20,
 		},
 	}
 
