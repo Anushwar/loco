b1dd972955e791982ee50c8a311874141b4b1b03
Anushwar Sharma <Anushwarsharma07@gmail.com>
2023-09-08 19:31:12 +0530

    test: add test case for closures

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -159,6 +159,17 @@ f(10);`,
 	}
 }
 
+func TestClousres(t *testing.T) {
+	input := `
+   let newAdder = fn(x) {
+     fn(y) { x + y };
+};
+   let addTwo = newAdder(2);
+   addTwo(2);`
+
+	testIntegerObject(t, testEval(input), 4)
+}
+
 func TestErrorHandling(t *testing.T) {
 	tests := []struct {
 		input           string
